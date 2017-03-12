8ec64c32ac207524971a2ba7bb8509e556c7108a
eldojk <eldojk@gmail.com>
2017-03-12 12:40:30 +0530

    TictacToe game tdd java kata finished

--- tddkatas/src/main/java/org/jklabs/tddkatas/TicTacToe.java
+++ tddkatas/src/main/java/org/jklabs/tddkatas/TicTacToe.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * TicTacToe game strategy
+ * 
+ * @author eldo.joseph
+ */
 public class TicTacToe {
 	public static final int E = 0;
 	public static final int X = 1;
@@ -18,9 +23,166 @@ public class TicTacToe {
 		priorityOfColumns.add(Arrays.asList(1, 3, 5, 7));
 	}
 
+	/**
+	 * Play the game. Returns the best possible cell to fill 
+	 * with the character given the current board position
+	 * 
+	 * @param board
+	 * @param character
+	 * @return
+	 */
 	public static int play(int[][] board, int character) {
+		// Return a possible win-able cell
+		int winnableCell = getWinnableCell(board, character);
+		
+		if (winnableCell != ERROR_CODE)
+			return winnableCell;
+		
+		// If no such cell exists, make sure opponent cannot win
+		int opponentCharacter = getOppnentCharacter(character);
+		int opponentWinnableCell = getWinnableCell(board, opponentCharacter);
+		
+		if (opponentWinnableCell != ERROR_CODE)
+			return opponentWinnableCell;
 
+		// Else Choose a random cell available with the highest priority
 		return getHighestPriorityFreeCell(board);
+	}
+
+	private static int getOppnentCharacter(int character) {
+		int opponentCharacter = O;
+		
+		if (character == O)
+			opponentCharacter = X;
+		
+		return opponentCharacter;
+	}
+
+	private static int getWinnableCell(int[][] board, 
+									   int character) {
+		int winningCell = getHorizontalWinnableCell(board, character);
+
+		if (winningCell != ERROR_CODE)
+			return winningCell;
+
+		winningCell = getVerticalWinnableCell(board, character);
+
+		if (winningCell != ERROR_CODE)
+			return winningCell;
+
+		return getDiagonalWinnableCell(board, character);
+	}
+
+	private static int getHorizontalWinnableCell(int[][] board, 
+												 int character) {
+		for (int row = 0; row < 3; row++) {
+			int characterCount = 0, emptyCellCount = 0, emptyCellI = 0, emptyCellJ = 0;
+
+			for (int col = 0; col < 3; col++) {
+
+				if (board[row][col] == character) {
+					characterCount++;
+
+				} else if (board[row][col] == E) {
+					emptyCellCount++;
+					emptyCellI = row;
+					emptyCellJ = col;
+				}
+			}
+
+			if ((characterCount == 2) && (emptyCellCount == 1)) {
+				return getCellValueFromCoordinates(emptyCellI, emptyCellJ);
+			}
+		}
+
+		return ERROR_CODE;
+	}
+
+	private static int getVerticalWinnableCell(int[][] board, 
+											   int character) {
+		for (int col = 0; col < 3; col++) {
+			int characterCount = 0, emptyCellCount = 0, emptyCellI = 0, emptyCellJ = 0;
+
+			for (int row = 0; row < 3; row++) {
+
+				if (board[row][col] == character) {
+					characterCount++;
+
+				} else if (board[row][col] == E) {
+					emptyCellCount++;
+					emptyCellI = row;
+					emptyCellJ = col;
+				}
+			}
+
+			if ((characterCount == 2) && (emptyCellCount == 1)) {
+				return getCellValueFromCoordinates(emptyCellI, emptyCellJ);
+			}
+		}
+
+		return ERROR_CODE;
+	}
+
+	private static int getDiagonalWinnableCell(int[][] board, 
+											   int character) {
+
+		int winnableCell = getWinnableCellInForwardDiagonal(board, character);
+		
+		if (winnableCell != ERROR_CODE)
+			return winnableCell;
+
+		return getWinnableCellInBackwardDiagonal(board, character);
+	}
+
+	private static int getWinnableCellInBackwardDiagonal(int[][] board,
+														 int character) {
+		int characterCount = 0, emptyCellCount = 0, emptyCellI = 0, emptyCellJ = 0;
+
+		for (int i = 0; i < 3; i++) {
+			int j = 3 - i - 1;
+			if (board[i][j] == character)
+				characterCount++;
+			else if (board[i][j] == E) {
+				emptyCellCount++;
+				emptyCellI = i;
+				emptyCellJ = j;
+			}
+
+		}
+
+		if ((characterCount == 2) && (emptyCellCount == 1)) {
+			return getCellValueFromCoordinates(emptyCellI, emptyCellJ);
+		}
+
+		return ERROR_CODE;
+	}
+
+	private static int getWinnableCellInForwardDiagonal(int[][] board,
+														int character) {
+		int characterCount = 0, emptyCellCount = 0, emptyCellI = 0, emptyCellJ = 0;
+
+		for (int i = 0; i < 3; i++) {
+			if (board[i][i] == character)
+				characterCount++;
+			else if (board[i][i] == E) {
+				emptyCellCount++;
+				emptyCellI = i;
+				emptyCellJ = i;
+			}
+
+		}
+
+		if ((characterCount == 2) && (emptyCellCount == 1)) {
+			return getCellValueFromCoordinates(emptyCellI, emptyCellJ);
+		}
+
+		return ERROR_CODE;
+	}
+
+	private static int getCellValueFromCoordinates(int emptyCellI, 
+												   int emptyCellJ) {
+
+		return (3 * emptyCellI) + emptyCellJ;
 	}
 
 	private static int getHighestPriorityFreeCell(int[][] board) {
@@ -38,7 +200,8 @@ public class TicTacToe {
 		return -1;
 	}
 
-	private static boolean isCellFree(int[][] board, int cellId) {
+	private static boolean isCellFree(int[][] board, 
+									  int cellId) {
 		int i = 0;
 		while (cellId >= 3) {
 
--- tddkatas/src/test/java/org/jklabs/tddkatas/TicTacToeTest.java
+++ tddkatas/src/test/java/org/jklabs/tddkatas/TicTacToeTest.java
@@ -33,6 +33,17 @@ public class TicTacToeTest {
 	}
 	
 	@Test
+	public void higherPriorityCellsTaken_onPlay_takesNextHigherPriorityCell() {
+		int[][] board = new int[][]{
+				new int[] {O, E ,O},
+				new int[] {E, O, E},
+				new int[] {X, E, X}
+		};
+		
+		assertTrue(Arrays.asList(1, 3, 5, 7).contains(TicTacToe.play(board, X)));
+	}
+	
+	@Test
 	public void fullBoard_onPlay_returnsErrorCode() {
 		int[][] board = new int[][]{
 				new int[] {X, O ,X},
@@ -41,5 +52,60 @@ public class TicTacToeTest {
 		};
 		
 		assertEquals(TicTacToe.play(board, X), ERROR_CODE);
+	}
+	
+	@Test
+	public void winnableHorizontalCellsExist_onPlay_winsTheGame() {
+		int[][] board = new int[][]{
+				new int[] {O, E ,O},
+				new int[] {E, O, E},
+				new int[] {X, E, X}
+		};
+		
+		assertEquals(TicTacToe.play(board, X), 7);
+	}
+	
+	@Test
+	public void winnableVerticalCellExist_onPlay_winsTheGame() {
+		int[][] board = new int[][]{
+				new int[] {O, X ,O},
+				new int[] {E, X, E},
+				new int[] {E, E, E}
+		};
+		
+		assertEquals(TicTacToe.play(board, X), 7);
+	}
+	
+	@Test
+	public void winnableForwardDiagonalCellExist_onPlay_winsTheGame() {
+		int[][] board = new int[][]{
+				new int[] {X, E ,E},
+				new int[] {O, X, E},
+				new int[] {O, E, E}
+		};
+		
+		assertEquals(TicTacToe.play(board, X), 8);
+	}
+	
+	@Test
+	public void winnableBackwardDiagonalCellExist_onPlay_winsTheGame() {
+		int[][] board = new int[][]{
+				new int[] {E, O ,E},
+				new int[] {O, X, E},
+				new int[] {X, E, E}
+		};
+		
+		assertEquals(TicTacToe.play(board, X), 2);
+	}
+	
+	@Test
+	public void opponentWinnableCellExist_onPlay_stopsOpponentFromWinning() {
+		int[][] board = new int[][]{
+				new int[] {O, X ,E},
+				new int[] {E, O, E},
+				new int[] {X, E, E}
+		};
+		
+		assertEquals(TicTacToe.play(board, X), 8);
 	}
 }
